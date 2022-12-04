1ba5ac4a50e9d527833d32261d96262cf60ce788
Abdallah-Helal <abdallahelal102@gmail.com>
2022-12-04 23:14:22 +0200

    third version

--- src/Services/DonationRechargeService.java
+++ src/Services/DonationRechargeService.java
@@ -3,6 +3,7 @@ import ServiceProviderFactorys.DonationProviderFactory;
 public class DonationRechargeService extends Service{
    
     public DonationRechargeService(){
+        this.name="DonationRecharge";
         this.servicefactory=new DonationProviderFactory();
         this.provider=servicefactory.createServiceProvider();
     }
--- src/Services/MobileRechargeService.java
+++ src/Services/MobileRechargeService.java
@@ -4,6 +4,7 @@ import ServiceProviderFactorys.MobileProviderFactory;
 
 public class MobileRechargeService extends Service{
     public MobileRechargeService(){
+        this.name="MobileRecharge";
         this.servicefactory=new MobileProviderFactory();
         this.provider=servicefactory.createServiceProvider();
     }
